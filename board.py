3d4bd812040dcaedda7a5400d540f11488b45e45
JP-Carr <j.p.carr@lancaster.ac.uk>
2021-03-02 01:27:23 +0000

    added path function
    Provides all squares covered in a move between 2 squares

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
-from numpy import array, zeros
+from numpy import array, zeros, arange, rot90
+import numpy as np
 
 class board:
     def __init__(self,x=8, y=None):
@@ -24,16 +25,13 @@ class board:
 
         Returns
         -------
-        bool
-            Returns True if occupied.
+        int
+            Returns the value of the position.
 
         """
-      #  print(position[0])
-      #  print(self.grid[position[0]][position[1]])
-        if self.grid[position[0]][position[1]]==0:
-            return False
-        else:
-            return True
+
+        return self.grid[position[0]][position[1]]
+
         
     def move_piece(self, old_position, new_position):
      #   print(new_position)
@@ -45,13 +43,59 @@ class board:
         self.grid[new_position[0]][new_position[1]]=self.grid[old_position[0]][old_position[1]]
         self.grid[old_position[0]][old_position[1]]=0
         return kill_id
+    
     def setup(self, position,_id):
         self.grid[position[0]][position[1]]=_id
+        
+    def path(self, start, finish):
+        
+        if start[0]<finish[0]:
+            n_spaces=arange(start[0],finish[0], dtype=int)
+        else:
+            n_spaces=arange(start[0],finish[0], -1, dtype=int)
+  
+        if start[1]<finish[1]:
+            m_spaces=arange(start[1],finish[1], dtype=int)
+        else:
+            m_spaces=arange(start[1],finish[1], -1, dtype=int)
+     
+            
+        #n_spaces=arange(min(start[0],finish[0]),max(start[0],finish[0]), dtype=int)
+        #m_spaces=arange(min(start[1],finish[1]),max(start[1],finish[1]), dtype=int)
+  #      print(n_spaces)
+   #     print(m_spaces)
+    #    print("\n")
+    
+        if len(n_spaces)==0:
+            n_spaces=np.ones((1,max(len(n_spaces),len(m_spaces))), dtype=int)*start[0]
+        elif len(m_spaces)==0:
+            m_spaces=np.ones((1,max(len(n_spaces),len(m_spaces))), dtype=int)*start[1]
+        spaces=zeros((2,max(len(n_spaces),len(m_spaces))), dtype=int)    
+
+        spaces[0,:]=n_spaces
+        spaces[1,:]=m_spaces
+
+
+        path=rot90(spaces)
+
+        has_start,has_finish=False,False
+        for i in path:
+            if all(i==start):
+                has_start=True
+            if all(i==finish):
+                has_finish=True
+        if has_start==True:
+            index=np.where((path == start).all(axis=1))[0][0]
+            path=np.delete(path,index,0)
+        if has_finish==False:
+            path=np.vstack((finish,path))
+        path=rot90(path, 2)
+        return path
     
 if __name__=="__main__":       
     x=board()
     x.grid[0][0]=5
-    x.grid[1][1]=3
-    print(x)
-    print(x.move_piece((0,0), (1,1)))
-    print(x)
+    x.grid[2][2]=3
+    print(x.path(array([0,0]), array([5,5])))
+    #print(x.path(array([5,5]), array([0,0])))
+
